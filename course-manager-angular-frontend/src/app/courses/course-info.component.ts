6192fb03f532aac3e967ad53cb8b702a408b54d7
Emerson Claudelino <emerson.claudelino@sinqia.com.br>
2020-06-01 22:16:27 -0300

    Criação server node

---
+++ course-manager-angular-frontend/src/app/courses/course-info.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CourseSerive } from './course.service';
+import { Course } from './course';
+
+@Component({
+    templateUrl: './course-info.component.html'
+})
+
+export class CourseInfoComponent implements OnInit {
+
+    course: Course;
+
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseSerive) {
+
+    }
+
+    ngOnInit(): void {
+        // + convert to number
+        this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
+            next: course => this.course = course,
+            error: err => console.log('Error', err)
+        });
+    }
+
+    save(): void{
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log('Saved with sucsess', course),
+            error: err => console.log('Error', err)
+        });
+    }
+
+}
---
+++ course-manager-angular-frontend/src/app/courses/course-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+import { CourseSerive } from './course.service';
+
+@Component({
+    templateUrl: './course-list.component.html'
+})
+
+export class CourseListComponet implements OnInit {
+
+    filteredCourses: Course[]=[];
+
+    _courses: Course[] = [];
+    _filterBy: string;
+
+    constructor(private courseService: CourseSerive) {
+
+    }
+
+    ngOnInit(): void {
+        this.retrieveAll();
+    }
+
+    retrieveAll(): void {
+         this.courseService.retrieveAll().subscribe({
+             next: courses => {
+                this._courses = courses;
+                this.filteredCourses = this._courses;
+             },
+             error: err => console.log('Error', err)
+         });        
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name
+                .toLocaleLowerCase()
+                .indexOf(this._filterBy.toLocaleLowerCase()) > -1)
+    }
+
+    get filter(){
+        return this._filterBy;
+    }
+
+
+}
